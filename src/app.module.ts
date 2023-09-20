1e22a0719b9b878429836c76c939bd48d02a23e6
Denis Kalkopf <deniskalkopf@gmail.com>
2023-09-20 10:49:17 +0500

    add mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TrackModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://deniskalkopf:denisjet@cluster0.hf7ycbu.mongodb.net/spotify-clone?retryWrites=true&w=majority',
+    ),
+    TrackModule,
+  ],
 })
 export class AppModule {}
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { Track, TrackDocument } from './schemas/track.schema';
+import { Comment, CommentDocument } from './schemas/comment.schema';
 
 @Injectable()
 export class TrackService {
