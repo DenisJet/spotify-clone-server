d57666b14669286c7364929365b84ce466a42d51
Denis Kalkopf <deniskalkopf@gmail.com>
2023-09-21 21:48:31 +0500

    add REST Api (WIP)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TrackModule } from './track/track.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGODB_URI), TrackModule],
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,14 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { Track, TrackDocument } from './schemas/track.schema';
 import { Comment, CommentDocument } from './schemas/comment.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { CreateTrackDto } from './dto/create-track.dto';
 
 @Injectable()
 export class TrackService {
-  async create() {}
+  constructor(
+    @InjectModel(Track.name) private trackModel: Model<Track>,
+    @InjectModel(Comment.name) private commentModel: Model<Comment>,
+  ) {}
 
-  async getAll() {}
+  async create(dto: CreateTrackDto): Promise<Track> {
+    const track = await this.trackModel.create({ ...dto, listens: 0 });
+    return track;
+  }
 
-  async getOne() {}
+  async getAll(): Promise<Track[]> {
+    const tracks = await this.trackModel.find();
+    return tracks;
+  }
 
-  async delete() {}
+  async getOne(id: ObjectId): Promise<Track> {
+    const track = await this.trackModel.findById(id);
+    return track;
+  }
+
+  async delete(id: ObjectId): Promise<ObjectId> {
+    const track = await this.trackModel.findByIdAndDelete(id);
+    return track._id;
+  }
 }
